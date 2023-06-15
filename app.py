82635619a128b1b69a445b73cfd26e46b6b2fd9d
Param Choksi <62841801+paramchoksi@users.noreply.github.com>
2023-06-15 10:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import  Flask
-app = Flask('hello')
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
@@ -7,4 +7,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run()
